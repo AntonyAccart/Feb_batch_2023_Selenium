e901f02420510c4b921a1c96a1ff750ff569ef00
AntonyAccart <126249726+AntonyAccart@users.noreply.github.com>
2023-03-22 16:53:31 +0530

    Add files via upload

---
+++ week5day2ass/system/Computer.java
@@ -0,0 +1,9 @@
+package system;
+
+//base class
+public class Computer {
+	public void computerModel() {
+		System.out.println("Computer Model is ASUS VivoBook");
+	}
+}
+
---
+++ week5day2ass/system/Desktop.java
@@ -0,0 +1,17 @@
+package system;
+
+//derived class
+public class Desktop extends Computer {
+	public void desktopSize() {
+		System.out.println("Size of the desktop is 25inches.");
+	}
+
+	public static void main(String[] args) {
+		Desktop desktop = new Desktop();
+		// calling method from Computer class
+		desktop.computerModel();
+		// calling method from Desktop class
+
+		desktop.desktopSize();
+	}
+}
