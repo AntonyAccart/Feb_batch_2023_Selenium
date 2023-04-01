3e3b40491850051adceeacdf0ff5d947c7fd9343
AntonyAccart <126249726+AntonyAccart@users.noreply.github.com>
2023-04-01 09:18:51 +0530

    Update RemoveDuplicate.java

--- week6day3ass/RemoveDuplicate.java
+++ week6day3ass/RemoveDuplicate.java
@@ -1,28 +1,38 @@
 package week6day3ass;
 
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
-
-
 public class RemoveDuplicate {
 public static void main(String[] args) {
-	int[] num= {1,2,3,4,5,6,6,2,4,};
+	String text = "PayPal India";
 	
-	Set<Integer> val=new LinkedHashSet<Integer>();
-	
-	for (int i = 0; i < num.length; i++) {
-		if(!val.add(num[i])) {
-			System.out.println(num[i]);
+	String txt = text.replaceAll("\\s", "");
+	String lowerCase = txt.toLowerCase();
+	char[] charArray = lowerCase.toCharArray();
+    Set<Character> charSet = new LinkedHashSet<Character>();
+    Set<Character> dupChar = new LinkedHashSet<Character>();
+for (Character charac : charArray) {
+	boolean add = charSet.add(charac);
+	if(add==false) {
+		dupChar.add(charac);
+	}
+}
+System.out.println("Actual text"+charSet);
+System.out.println("Duplicated text"+dupChar);
+List<Character> list = new ArrayList<Character>(charSet);
+List<Character> list1 = new ArrayList<Character>(dupChar);
+for (int i = 0; i < list.size(); i++) {
+	for (int j = 0; j < list1.size(); j++) {
+		if(list.get(i)==list1.get(j)) {
+			list.remove(list.get(i));
 		}
 	}
-	
-	System.out.println(val);
-	
-	
-	
+}
+System.out.println("Unique text"+list);
+
 }
 }
